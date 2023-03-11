ab38b71810d90b69c68bff14746b50f053da849e
Hussain <lodhgerhussain@gmail.com>
2023-03-10 22:02:12 -0800

    Updated about and views.index in views.py

--- textutils/views.py
+++ textutils/views.py
@@ -2,7 +2,8 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello Hussain")
+    return HttpResponse('''<h1> Hussain </h1>
+                            <a href ="https://github.com/"> GitHub </a>''')
 
 def about(request):
     return HttpResponse("<h1>This is about hussain</h1>")
