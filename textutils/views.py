7b1ecf9216a76fa0bfd7083f49625dee96625ce6
Hussain <lodhgerhussain@gmail.com>
2023-03-18 04:47:04 -0700

    Added Functionality to remove extra space and new line

--- textutils/views.py
+++ textutils/views.py
@@ -11,6 +11,8 @@ def index(request):
 def analyze(request):
     djtext = request.GET.get("Text", "default")
     removepunctuation = request.GET.get("removepunc", "Off")
+    newlineremove_text = request.GET.get("newlineremover", "Off")
+    extraspaceremover_text = request.GET.get("extaspaceremover", "Off")
     uppercase_char = request.GET.get("uppercase", "Off")
     analyzed = ""
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
@@ -28,6 +30,25 @@ def analyze(request):
             analyzed += char.upper()
 
         params = {"purpose": "make the text into upper case",
+                  "analyzed_text": analyzed}
+        return render(request, "analyze.html", params)
+
+    elif newlineremove_text == "on":
+        analyzed = ""
+        for char in djtext:
+            if char != "\n":
+                analyzed += char
+
+        params = {"purpose": "Remove New Line",
+                  "analyzed_text": analyzed}
+        return render(request, "analyze.html", params)
+
+    elif extraspaceremover_text == "on":
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not djtext[index] == " " and djtext[index+1] == " ":
+                analyzed += char
+        params = {"purpose": "Remove Extra space",
                   "analyzed_text": analyzed}
         return render(request, "analyze.html", params)
 
