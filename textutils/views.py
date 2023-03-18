28eb7b943a3d6918c614e5868cc0971acf35d958
Hussain <lodhgerhussain@gmail.com>
2023-03-17 21:55:48 -0700

    Added upper case functionality

--- textutils/views.py
+++ textutils/views.py
@@ -1,29 +1,40 @@
-#I have created this file 
+# I have created this file
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    
-    return render(request,'index.html')
+
+    return render(request, 'index.html')
+
 
 def analyze(request):
-    djtext = request.GET.get("Text","default")
-    removepunctuation = request.GET.get("removepunc","Off")
+    djtext = request.GET.get("Text", "default")
+    removepunctuation = request.GET.get("removepunc", "Off")
+    uppercase_char = request.GET.get("uppercase", "Off")
     analyzed = ""
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     if removepunctuation == "on":
         for char in djtext:
             if char not in punctuations:
-                analyzed+=char
+                analyzed += char
 
-    params = {"purpose": "Remove Punctuations","analyzed_text":analyzed}
-    return render(request,"analyze.html",params)
+        params = {"purpose": "Remove Punctuations", "analyzed_text": analyzed}
+        return render(request, "analyze.html", params)
+
+    elif uppercase_char == "on":
+        analyzed = ""
+        for char in djtext:
+            analyzed += char.upper()
+
+        params = {"purpose": "make the text into upper case",
+                  "analyzed_text": analyzed}
+        return render(request, "analyze.html", params)
 
 
 def exercise(request):
 
     return HttpResponse('''<a href = "www.facebook.com">Facebook</a> <br>
                             <a href = "www.Google.com">Google</a> <br>
-                            <a href = "www.Rediff.com">Rediff </a><br>
+                            <a href = "www.Rediff.com">Rediff</a><br>
                             <a href = "www.Yahoo.com">Yahoo</a> <br>''')
-
