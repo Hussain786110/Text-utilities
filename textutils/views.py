a4aa3e6460bd3f0aacb7992ace1bc244a5009b77
Hussain <lodhgerhussain@gmail.com>
2023-03-23 21:34:30 -0700

    made final adjsutments

--- textutils/views.py
+++ textutils/views.py
@@ -9,41 +9,39 @@ def index(request):
 
 
 def analyze(request):
-    djtext = request.GET.get("Text", "default")
-    removepunctuation = request.GET.get("removepunc", "Off")
-    newlineremove_text = request.GET.get("newlineremover", "Off")
-    extraspaceremover_text = request.GET.get("extaspaceremover", "Off")
-    uppercase_char = request.GET.get("uppercase", "Off")
+    djtext = request.POST.get("Text", "default")
+    removepunctuation = request.POST.get("removepunc", "Off")
+    newlineremove_text = request.POST.get("newlineremover", "Off")
+    extraspaceremover_text = request.POST.get("extaspaceremover", "Off")
+    uppercase_char = request.POST.get("uppercase", "Off")
     analyzed = ""
+    purpose_string = ""
     punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
     if removepunctuation == "on":
         for char in djtext:
             if char not in punctuations:
                 analyzed += char
+        djtext = analyzed
+        purpose_string += "Remove Punctuation |"
 
-        params = {"purpose": "Remove Punctuations", "analyzed_text": analyzed}
-        return render(request, "analyze.html", params)
-
-    elif uppercase_char == "on":
+    if uppercase_char == "on":
         analyzed = ""
         for char in djtext:
             analyzed += char.upper()
 
-        params = {"purpose": "make the text into upper case",
-                  "analyzed_text": analyzed}
-        return render(request, "analyze.html", params)
+        purpose_string += "Make the Text to Uppercase |"
+        djtext = analyzed
 
-    elif newlineremove_text == "on":
+    if newlineremove_text == "on":
         analyzed = ""
         for char in djtext:
-            if char != "\n":
+            if char != "\n" and char != "\r":
                 analyzed += char
 
-        params = {"purpose": "Remove New Line",
-                  "analyzed_text": analyzed}
-        return render(request, "analyze.html", params)
+        purpose_string += "Remove the new Lines |"
+        djtext = analyzed
 
-    elif extraspaceremover_text == "on":
+    if extraspaceremover_text == "on":
         analyzed = ""
         for index, char in enumerate(djtext):
             if (index+1) < len(djtext):
@@ -51,14 +49,11 @@ def analyze(request):
                     analyzed += char
             else:
                 analyzed += char
-        params = {"purpose": "Remove Extra space",
-                  "analyzed_text": analyzed}
+        purpose_string += "Remove Extra spaces "
+        djtext = analyzed
+
+    if removepunctuation != "on" and uppercase_char != "on" and newlineremove_text != "on" and extraspaceremover_text != "on":
+        return HttpResponse("Error")
+    else:
+        params = {"purpose": purpose_string, "analyzed_text": djtext}
         return render(request, "analyze.html", params)
-
-
-def exercise(request):
-
-    return HttpResponse('''<a href = "www.facebook.com">Facebook</a> <br>
-                            <a href = "www.Google.com">Google</a> <br>
-                            <a href = "www.Rediff.com">Rediff</a><br>
-                            <a href = "www.Yahoo.com">Yahoo</a> <br>''')
